37473e13e25971ca8c8da64010542a43ad52c60d
David Lee <cnlijiacheng96@gmail.com>
2017-11-26 19:56:39 +0800

    add conv_forward and conv_backward

--- zeronet/core/function.py
+++ zeronet/core/function.py
@@ -29,7 +29,7 @@ def linear_forward(x, weights):
     return out
 
 
-def linear_backward(dout, input, weights):
+def linear_backward(x, weights, dout):
     """
     Computes the backward pass for an affine layer.
 
@@ -44,7 +44,6 @@ def linear_backward(dout, input, weights):
     - dw: Gradient with respect to w, of shape (D, M)
     - db: Gradient with respect to b, of shape (M,)
     """
-    x = input
     w, b = weights
     dx, dw, db = None, None, None
     dx = np.dot(dout, w.T)
@@ -54,4 +53,86 @@ def linear_backward(dout, input, weights):
     x = x.reshape(batch_size, input_size)
     dw = np.dot(x.T, dout)
     db = np.dot(dout.T, np.ones(batch_size))
-    return dx, dw, db
+    grads = (dx, dw, db)
+    return grads
+
+
+def conv_forward(x, weights, conv_params):
+    '''
+        The input consists of N data points, each with C channels, height H and
+    width W. We convolve each input with F different filters, where each filter
+    spans all C channels and has height HH and width HH.
+
+    Input:
+    - x: Input data of shape (N, C, H, W)
+    - w: Filter weights of shape (F, C, HH, WW)
+    - b: Biases, of shape (F,)
+    - conv_param: A dictionary with the following keys:
+      - 'stride': The number of pixels between adjacent receptive fields in the
+        horizontal and vertical directions.
+      - 'pad': The number of pixels that will be used to zero-pad the input.
+
+    Returns a tuple of:
+    - out: Output data, of shape (N, F, H', W') where H' and W' are given by
+      H' = 1 + (H + 2 * pad - HH) / stride
+      W' = 1 + (W + 2 * pad - WW) / stride
+    - cache: (x, w, b, conv_param)
+    '''
+    out = None
+    w, b = weights
+    N, C, H, W = x.shape
+    F, _, HH, WW = w.shape
+    stride, pad = conv_param['stride'], conv_param['pad']
+    H_out = int(1 + (H + 2 * pad - HH) / stride)
+    W_out = int(1 + (W + 2 * pad - WW) / stride)
+    out = np.zeros((N , F , H_out, W_out))
+
+    x_pad = np.pad(x, ((0,), (0,), (pad,), (pad,)), mode='constant', constant_values=0)
+    for i in range(H_out):
+        for j in range(W_out):
+            x_pad_masked = x_pad[:, :, i*stride:i*stride+HH, j*stride:j*stride+WW] # slide window
+            for k in range(F): # # of filters
+                out[:, k, i, j] = np.sum(x_pad_masked * w[k, :, :, :], axis=(1,2,3))
+
+    out = out + (b)[None, :, None, None] # add bias
+    return out
+
+
+
+def conv_backward(x, weights, conv_params, dout):
+    '''
+    Inputs:
+    - dout: Upstream derivatives.
+
+    Returns a tuple of:
+    - dx: Gradient with respect to x
+    - dw: Gradient with respect to w
+    - db: Gradient with respect to b
+    '''
+
+    w, b = weights
+    N, C, H, W = x.shape
+    F, _, HH, WW = w.shape
+    stride, pad = conv_param['stride'], conv_param['pad']
+    H_out = int(1 + (H + 2 * pad - HH) / stride)
+    W_out = int(1 + (W + 2 * pad - WW) / stride)
+    x_pad = np.pad(x, ((0,), (0,), (pad,), (pad,)), mode='constant', constant_values=0)
+
+    dx = np.zeros_like(x)
+    dx_pad = np.zeros_like(x_pad)
+    dw = np.zeros_like(w)
+    db = np.zeros_like(b)
+
+    db = np.sum(dout, axis(0, 2, 3))
+
+    for i in range(H_out):
+        for j in range(W_out):
+            x_pad_masked = x_pad[:, :, i*stride:i*stride+HH, j*stride:j*stride+WW]
+            for k in range(F):
+                dw[k, :, :, :] += np.sum(x_pad_masked * (dout[:, k, i, j])[:, None, None, None], axis=0) #?
+            for n in range(N):
+                dx_pad[n, :, i*stride:i*stride+HH, j*stride:j*stride+WW] += np.sum((w[:, :, :, :] * 
+                                                                    (dout[n, :, i, j])[:, None, None, None]), axis=0)
+    dx = dx_pad[:, :, pad:-pad, pad:-pad]
+    grads = (dx, dw, db)
+    return grads
