441a23ae4d75b8bd90a665bf050b710008f6f2df
ddlee <cnlijiacheng96@gmail.com>
2017-12-05 00:38:24 +0800

    add regularization in net object

--- zeronet/core/net.py
+++ zeronet/core/net.py
@@ -1,13 +1,14 @@
 # Author: David Lee, me@ddlee.cn
 from zeronet.core.layer import *
 from zeronet.core.function import *
+import numpy as np
 
 class net(object):
     '''
     the net class is the abstraction of DAG composed by stacked layers
     it call forward and backward methods of layers recursively'''
 
-    def __init__(self, layer_stack, loss_func):
+    def __init__(self, layer_stack, loss_func, reg):
         '''
         layer stack is a list of layers with specific order
         layer stack start with a Input Layer, loss function not included
@@ -15,6 +16,7 @@ class net(object):
         self.layer_stack = layer_stack
         self.layer_mount = len(layer_stack)
         self.loss_func = loss_func
+        self.reg = reg
         self.input_dict = None
         self.optim_configs = None
         self.params = None
@@ -68,13 +70,21 @@ class net(object):
         '''
         out = self.forward(X_batch)
         loss, dout = self.loss_func(out, y_batch)
+        # add regularization
+        reg_loss = 0
+        for layer_param in self.params.values():
+            for value in layer_param.values():
+                reg_loss += np.sum(value ** 2)
+        loss += self.reg * 0.5 * reg_loss
         return (loss, dout)
 
-    def backward(self, optimizer, loss, dout):
+    def backward(self, optimizer, dout):
         '''
         perform back propagation and update params recurstively
         currently, just reverse the layer stack and caculate from last layer
         '''
+        # wrap dout from loss_function
+        dout = dict({'x': dout})
         for _layer in self.layer_stack[::-1]:
-            dout = _layer.grad(self.input_dict[_layer], dout['dx'])
+            dout = _layer.grad(self.input_dict[_layer.name], dout['x'])
             self.optim_configs = _layer.update(dout, optimizer, self.optim_configs)
